bb4669a97e33482e66455a5e417fca9375f94e0b
Petycomp <peter@oroszvari.hu>
2017-12-27 20:44:12 +0000

    basics"

---
+++ database.py
@@ -0,0 +1,33 @@
+from mysql.connector import MySQLConnection, Error
+from mysql_dbconfig import read_db_config
+ 
+ 
+def insert_wallet(pubkey,privkey):
+    """ Connect to MySQL database """
+    query = "INSERT INTO vertcoin_wallet(pubkey,privkey) " \
+            "VALUES(%s,%s)" 
+    args = (pubkey, privkey)
+
+    try:
+	db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+ 
+        if conn.is_connected():
+            print('connection established.')
+	    cursor = conn.cursor()
+    	    cursor.execute(query, args)
+	    conn.commit()
+
+        else:
+            print('connection failed.')
+ 
+    except Error as error:
+        print(error)
+ 
+    finally:
+        conn.close()
+        print('Connection closed.')
+ 
+ 
+if __name__ == '__main__':
+    connect()
---
+++ wallet.py
@@ -0,0 +1,29 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from wallet import 
+
+def connect(username, password):
+    rpc_password = '1234'
+    global rpc_connection
+    rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:5888"%(username, password))
+    
+
+def create_wallet():
+    wallet = rpc_connection.getnewaddress()
+    privkey = rpc_connection.dumpprivkey(wallet)
+    print "Your new wallet is:" + wallet
+    print "Your privkey is" + privkey
+
+
+def balance():
+    print "Your balance is %f" % (rpc_connection.getbalance())
+    print "Your other balance is %f" % (rpc_connection.getreceivedbyaddress("VrR8YGqcGVt4F9ZKPYKBpiAviLfgoByr49"))
+
+
+def main():
+    connect('cheppers','1234')
+    create_wallet()
+ 
+if __name__ == '__main__':
+    main()
+
+
