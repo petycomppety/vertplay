dea74381e4d3d03d83bc41587debdeaac53e44a4
Peter Oroszvari <peter.oroszvari@cheppers.com>
2017-12-28 18:29:15 +0100

    database play

--- database.py
+++ database.py
@@ -24,7 +24,6 @@ def mysql_connect(query, args):
         print('Connection closed.')
 
 def insert_wallet(pubkey,privkey):
-    """ Connect to MySQL database """
     query = "INSERT INTO vertcoin_wallet(pubkey,privkey) " \
             "VALUES(%s,%s)" 
     args = (pubkey, privkey)
@@ -33,4 +32,21 @@ def insert_wallet(pubkey,privkey):
    
 def read_wallets():
     query = "SELECT * from vertcoin_wallet"
-    
+    try:
+	    db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+        if conn.is_connected():
+            print('connection established.')
+	        cursor.execute(query)
+	        numrows = cursor.rowcount
+            for x in xrange(0,numrows)
+                print cursor.fetchone()
+        else:
+            print('connection failed.')
+ 
+    except Error as error:
+        print(error)
+ 
+    finally:
+        conn.close()
+        print('Connection closed.')
